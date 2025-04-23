e8bf231087196ad0a40a82df181b7c8b0852905e
Daniel Jaworski <daniel.jaworski@emoti.group>
2025-04-23 22:48:22 +0200

    feat: run more workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   reporter: process.env.CI ? "github" : [["html", { outputFolder: "./playwright/report" }], ["list"]],
   use: {
     baseURL: "http://localhost:3000",
